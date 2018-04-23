4009374c8ecacde193847acbec80a8d1312876cd
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,12 @@
+# How to filter XtraReport data based on the selected rows in the ASPxGridView
+
+
+<p>This example demonstrates how to filter XtraReport data based on the selected rows in the ASPxGridView. The XtraReport is placed into a separate ASPxPopupControl window. End users have the capability to resize this window and move it within a containment web page. This window is hidden automatically when there are no selected rows in the ASPxGridView.</p><p>Some key points of this example are:</p><p>1) The client ASPxClientGridView.SelectionChanged event is handled to track the rows selection.<br />
+2) The client ASPxClientGridView.GetSelectedFieldValues() method is called to retrieve IDs of the selected rows.<br />
+3) Selected row IDs are saved to the HiddenField, so that they will be available later on the server.<br />
+4) The callback to the server initiates via the client ASPxClientReportViewer.Refresh() method.<br />
+5) The IDs, previously saved in the HiddenField, are used to form the expression for the XtraReport.FilterString property.</p>
+
+<br/>
+
+
---
+++ VB/Default.aspx.vb
@@ -0,0 +1,53 @@
+﻿Imports System
+Imports DevExpress.Web.ASPxGridView
+Imports nwndDataSetTableAdapters
+
+Partial Public Class _Default
+	Inherits System.Web.UI.Page
+
+	Protected Sub Page_Load(ByVal sender As Object, ByVal e As EventArgs)
+		Dim ds As nwndDataSet = GetData()
+
+		ASPxGridView1.DataSource = ds
+		ASPxGridView1.DataMember = ds.Categories.TableName
+
+		ASPxGridView1.DataBind()
+
+		Dim report As XtraReport1 = GetReport()
+		Dim selectedIDs As String = HiddenField1.Value.Trim(New Char() { ","c })
+
+		report.FilterString = String.Format("CategoryID in ({0})", selectedIDs)
+		report.CreateDocument()
+		ReportViewer1.Report = report
+	End Sub
+
+	Protected Sub ReportViewer1_Unload(ByVal sender As Object, ByVal e As EventArgs)
+		ReportViewer1.Report = Nothing
+	End Sub
+
+	Protected Function GetReport() As XtraReport1
+		If DirectCast(Session("report"), XtraReport1) Is Nothing Then
+			Dim report As New XtraReport1()
+			report.DataSource = GetData()
+
+			Session("report") = report
+		End If
+
+		Return DirectCast(Session("report"), XtraReport1)
+	End Function
+
+	Protected Function GetData() As nwndDataSet
+		If DirectCast(Session("dataset"), nwndDataSet) Is Nothing Then
+			Dim ds As New nwndDataSet()
+			Dim cta As New CategoriesTableAdapter()
+			Dim pta As New ProductsTableAdapter()
+
+			cta.Fill(ds.Categories)
+			pta.Fill(ds.Products)
+
+			Session("dataset") = ds
+		End If
+
+		Return DirectCast(Session("dataset"), nwndDataSet)
+	End Function
+End Class
